a2a83e122ca1854bf58aa57acf26d7a14fd96f77
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2025-09-03 16:51:07 +0600

    Fix: Try 1 of login system

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:5000/api/v1',
+  baseURL: process.env.API_URL,
   withCredentials: true,
 });
 
